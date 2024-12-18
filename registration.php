c05821d47872fdb664d250d58215851e53df5733
Abdelkarim Fares <abdelkarim.fares@cartware.de>
2024-12-18 14:23:59 +0100

    initialize module-ad-manager

---
+++ registration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2024 NIJI
+ * All rights reserved
+ *
+ * This product includes proprietary software developed at NIJI COMPANY
+ *
+ * To obtain a valid license for using this software please contact us at
+ * license@niji.com
+ *
+ * @package    Niji_Ad_Manager
+ * @copyright  Copyright (c) 2024 NIJI
+ * @author     abdelkarim fares <abdelkarimfares94@gmail.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Niji_Ad_Manager',
+    __DIR__
+);
